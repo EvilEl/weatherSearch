d8f328f865d4f6a2841f882cb974763b6fa91ebd
EvilEl <enc0ru@rambler.ru>
2021-10-23 18:47:30 +0300

    added csp

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  render: {
+    csp: true
+  },
+
   head: {
     title: 'weather',
     htmlAttrs: {
@@ -9,7 +13,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
